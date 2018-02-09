9de432bbb8909b8e5672bca07aa7c0873ea92c6b
Mark <markhenningerwork@gmail.com>
2018-02-09 16:11:52 +0100

    add createComment functionality

--- server.js
+++ server.js
@@ -28,17 +28,17 @@ const routes = {
   },
   '/articles/:id/downvote': {
     'PUT': downvoteArticle
-  }
+  },
   '/comments': {
     'POST': createComment
-  }
+  },
   '/comments/:id': {
     'PUT': updateComment,
     'DELETE': deleteComment
-  }
+  },
   '/comments/:id/upvote': {
     'PUT': upvoteComment
-  }
+  },
   '/comments/:id/downvote': {
     'PUT': downvoteComment
   }
@@ -253,6 +253,51 @@ function downvote(item, username) {
     item.downvotedBy.push(username);
   }
   return item;
+}
+
+function createComment(url, request) {
+  //copied from createArticle and edited for comment
+  const requestComment = request.body && request.body.comment;
+  const response = {};
+
+  if (requestComment && database.articles[requestComment.articleId] && requestComment.body && requestComment.username && database.users[requestComment.username]) {
+    const comment = {
+      id: database.nextCommentId++,
+      body: requestComment.body,
+      articleId: requestComment.articleId,
+      username: requestComment.username,
+      upvotedBy: [],
+      downvotedBy: []
+    };
+
+    database.comments[comment.id] = comment;
+    database.users[comment.username].commentIds.push(comment.id);
+    database.articles[comment.articleId].commentIds.push(comment.id);
+
+    response.body = {comment: comment};
+    response.status = 201;
+  } else {
+    response.status = 400;
+  }
+
+  return response;
+}
+
+function updateComment(url, request) {
+
+
+}
+
+function deleteComment(url, request) {
+
+}
+
+function upvoteComment(url, request) {
+
+}
+
+function downvoteComment(url, request) {
+
 }
 
 // Write all code above this line.
